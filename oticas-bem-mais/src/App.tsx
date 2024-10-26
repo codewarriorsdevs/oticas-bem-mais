e278ebd160823026f02e3bde885fed66384f8cf0
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2024-10-26 16:18:23 -0300

    feat: main configurations to project.

---
+++ oticas-bem-mais/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center text-blue-500 font-bold">
+      <h1>Hello, Tailwind CSS with Vite and React!</h1>
+    </div>
+  );
+}
+
+export default App;
